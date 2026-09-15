3e09f4b3ad2285034e286076464b139bd7f5a13b
SujibShrestha <sujeeb98@gmail.com>
2026-09-15 21:30:40 +0545

    Add Tailwind CSS integration and explored Admin page

--- billing/urls.py
+++ billing/urls.py
@@ -24,4 +24,7 @@ urlpatterns = [
     path('about/',views.about, name='about'),
     path('contact/',views.contact, name='contact'),
     path('myapp/', include('myapp.urls')),  # Include the URLs from myapp
+
+
+    path("__reload__/", include("django_browser_reload.urls")),  # Add this line for django-browser-reload
 ]
