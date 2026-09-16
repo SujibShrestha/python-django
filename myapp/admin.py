5069114b9ec11be2e55d0f950a524c92e3f2d318
SujibShrestha <sujeeb98@gmail.com>
2026-09-16 19:18:52 +0545

    Add models for appReview, Store, and appCertificate with admin integration and Looked admin

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
-from .models import appVariety
+from .models import appVariety, appReview, Store, appCertificate
 
 # Register your models here.
-admin.site.register(appVariety)
+class AppReviewInline(admin.TabularInline):
+    model = appReview
+    extra = 2
+
+class AppVarietyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'date')
+    inlines = [AppReviewInline]
+
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location')
+    filter_horizontal = ('app_varieties',)
+
+class AppCertificateInline(admin.ModelAdmin):
+    list_display = ('certificate_number', 'issued_date', 'valid_until')
+
+
+
+admin.site.register(appVariety , AppVarietyAdmin)
+admin.site.register(Store, StoreAdmin)
+admin.site.register(appCertificate, AppCertificateInline)
--- myapp/models.py
+++ myapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 # Create your models here.
 
 class appVariety(models.Model):
@@ -16,3 +17,40 @@ class appVariety(models.Model):
 
     def __str__(self):
         return self.name 
+
+
+
+# 1 - *
+
+class appReview(models.Model):
+    app = models.ForeignKey(appVariety,on_delete=models.CASCADE,related_name='reviews')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.FloatField(max_length=5.0)
+    comment = models.TextField(max_length=500, blank=True)
+    date = models.DateTimeField(default= timezone.now)
+
+
+    def __str__(self):
+        return f'{self.user.username} reviewed {self.app.name} with rating {self.rating}'
+
+
+# many to many
+
+class Store(models.Model):
+    name = models.CharField(max_length = 100)
+    location = models.CharField(max_length = 100)
+    app_varieties = models.ManyToManyField(appVariety, related_name='stores')
+
+    def __str__(self):
+        return self.name
+
+#one to one
+
+class appCertificate(models.Model):
+    app = models.OneToOneField(appVariety, on_delete=models.CASCADE, related_name='certificate')
+    certificate_number = models.CharField(max_length=100)
+    issued_date = models.DateTimeField(default=timezone.now)
+    valid_until = models.DateTimeField()
+
+    def __str__(self):
+        return f'Certificate {self.certificate_number} for {self.app.name}'
